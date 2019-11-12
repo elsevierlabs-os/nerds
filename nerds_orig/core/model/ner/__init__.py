aa7b2571693aad9cddb05082a971f7d234785505
sujitpal <sujit.pal@comcast.net>
2019-11-12 13:09:43 -0800

    adding ExactMatchMultiClassDictionaryNER entry to __init__.py

--- nerds_orig/core/model/ner/__init__.py
+++ nerds_orig/core/model/ner/__init__.py
@@ -2,6 +2,7 @@ from nerds.core.model.ner.base import NERModel
 from nerds.core.model.ner.bilstm import BidirectionalLSTM
 from nerds.core.model.ner.crf import CRF
 from nerds.core.model.ner.dictionary import ExactMatchDictionaryNER
+from nerds.core.model.ner.dictionary import ExactMatchMultiClassDictionaryNER
 from nerds.core.model.ner.ensemble import NERModelEnsemble
 from nerds.core.model.ner.spacy import SpaCyStatisticalNER
 
@@ -9,6 +10,7 @@ __all__ = [
     "BidirectionalLSTM",
     "CRF",
     "ExactMatchDictionaryNER",
+    "ExactMatchMultiClassDictionaryNER",
     "NERModel",
     "NERModelEnsemble",
     "SpaCyStatisticalNER"
