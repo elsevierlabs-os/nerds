3025e858131f5a798cf2f34dbabd2edbd71d3233
sujitpal <sujit.pal@comcast.net>
2019-11-22 07:49:50 -0800

    also remove zip file

--- examples/GMB/download_glove.sh
+++ examples/GMB/download_glove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip -a glove.6B.zip
-rm glove.6B.200d.txt glove.6B.300d.txt glove.6B.50d.txt
+rm glove.6B.200d.txt glove.6B.300d.txt glove.6B.50d.txt glove.6B.zip
